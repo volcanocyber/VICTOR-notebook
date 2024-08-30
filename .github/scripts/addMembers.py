652e47f990e9390face701c96b44fd2a2cb20fa6
Sam Krasnoff <victor@ldeo.columbia.edu>
2024-08-30 10:51:18 -0400

    Streamlining of new user automation, and google earth engine additions for TROPOMI use

--- .github/scripts/addMembers.py
+++ .github/scripts/addMembers.py
@@ -1,4 +1,4 @@
-import gspread
+import json
 import pandas as pd
 import requests
 import datetime as dt
@@ -8,22 +8,21 @@ api = "https://api.github.com/"
 token = os.environ['GITHUB_TOKEN']
 
 def main():
-    gc = gspread.service_account()
-    sh = gc.open_by_key("1KUYomxvwUNAVoZrCvdTeIFm9jKe7l_g1Uv9bDLTH6wQ")
-    vals = pd.DataFrame(sh.get_worksheet(0).get_all_records())
-    usernames = vals["Github Username"].values
-    timestamp = vals["Timestamp"].values
-    days = []
-    for val in timestamp:
-        days.append(val.split()[0])
-    today = dt.date.today()-dt.timedelta(days=1)
-    d1 = today.strftime("%m/%d/%Y")
-    new_users = [usernames[i] for i in range(len(days)) if days[i]==d1]
-    for user in new_users:
-        endpoint = f"orgs/VICTOR-Community/teams/victoraccess/membership/{user}"
-        url = f"{api}{endpoint}"
-        response = requests.put(url, auth=("volcanocyber",token))
-        print(response.json())
+    with open('results.json', 'r') as file:
+        data = json.load(file)
+        usernames = data["Github Username"]
+        timestamp = data["Timestamp"]
+        days = []
+        for val in timestamp:
+            days.append(val.split()[0])
+        today = dt.date.today()-dt.timedelta(days=1)
+        d1 = today.strftime("%m/%d/%Y")
+        new_users = [usernames[i] for i in range(len(days)) if days[i]==d1]
+        for user in new_users:
+            endpoint = f"orgs/VICTOR-Community/teams/victoraccess/membership/{user}"
+            url = f"{api}{endpoint}"
+            response = requests.put(url, auth=("volcanocyber",token))
+            print(response.json())
         
 if __name__ == "__main__":
     main()
--- .github/scripts/decrypt_secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_SECRET" \
---output service-account.json service_account.json.gpg
